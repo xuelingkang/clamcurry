5f137a9a446bc34489626d7616006d0413d7eef8
xuelingkang <xuelingkang@163.com>
2024-01-07 22:48:09 +0800

    Fix: menu

--- packages/main/src/menu.ts
+++ packages/main/src/menu.ts
@@ -57,172 +57,273 @@ const handleFeedbackEvent = (menuItem: MenuItem) => {
     mainProcessService.openLink('https://github.com/xuelingkang/clamcurry/issues');
 };
 
-const isMac = process.platform === 'darwin';
-
-const menuTemplate = [
-    new MenuItem({
-        label: app.name,
-        visible: isMac,
-        enabled: isMac,
-        submenu: [
-            {
-                label: `About ${app.name}`,
-                role: 'about',
-            },
-            {
-                type: 'separator',
-            },
-            {
-                label: 'Preference Settings',
-                accelerator: 'command+,',
-                click: handlePreferenceMenuEvent,
-            },
-            {
-                label: 'Theme Settings',
-                accelerator: 'command+t',
-                click: handleThemeMenuEvent,
-            },
-            {
-                type: 'separator',
-            },
-            {
-                label: `Hide ${app.name}`,
-                role: 'hide',
-            },
-            {
-                type: 'separator',
-            },
-            {
-                label: `Quit ${app.name}`,
-                role: 'quit',
-            },
-        ],
-    }),
-    new MenuItem({
-        label: 'File',
-        submenu: [
-            {
-                label: 'Preference Settings',
-                visible: !isMac,
-                enabled: !isMac,
-                accelerator: 'ctrl+p',
-                click: handlePreferenceMenuEvent,
-            },
-            {
-                label: 'Theme Settings',
-                visible: !isMac,
-                enabled: !isMac,
-                accelerator: 'ctrl+t',
-                click: handleThemeMenuEvent,
-            },
-            {
-                type: 'separator',
-                visible: !isMac,
-            },
-            {
-                label: 'New Note',
-                accelerator: isMac ? 'command+n' : 'ctrl+n',
-                click: handleNewNoteEvent,
-            },
-            {
-                label: 'Search In Notebook',
-                accelerator: isMac ? 'command+shift+f' : 'ctrl+shift+f',
-                click: handleSearchNoteEvent,
-            },
-            {
-                label: 'Close Note',
-                accelerator: isMac ? 'command+w' : 'ctrl+w',
-                click: handleCloseNoteEvent,
-            },
-        ],
-    }),
-    new MenuItem({
-        label: 'Edit',
-        submenu: [
-            {
-                role: 'undo',
-            },
-            {
-                role: 'redo',
-            },
-            {
-                type: 'separator',
-            },
-            {
-                role: 'cut',
-            },
-            {
-                role: 'copy',
-            },
-            {
-                role: 'paste',
-            },
-            {
-                role: 'selectAll',
-            },
-        ],
-    }),
-    new MenuItem({
-        label: 'View',
-        submenu: [
-            {
-                label: 'Toggle Sidebar',
-                accelerator: isMac ? 'command+1' : 'ctrl+1',
-                click: handleToggleSidebarEvent,
-            },
-            {
-                label: 'Toggle Outline',
-                accelerator: isMac ? 'command+2' : 'ctrl+2',
-                click: handleToggleOutlineEvent,
-            },
-            {
-                type: 'separator',
-            },
-            {
-                role: 'reload',
-            },
-            {
-                role: 'forceReload',
-            },
-            {
-                role: 'toggleDevTools',
-            },
-            {
-                type: 'separator',
-            },
-            {
-                role: 'resetZoom',
-            },
-            {
-                role: 'zoomIn',
-            },
-            {
-                role: 'zoomOut',
-            },
-            {
-                type: 'separator',
-                visible: isMac,
-            },
-            {
-                role: 'togglefullscreen',
-                visible: isMac,
-                enabled: isMac,
-            },
-        ],
-    }),
-    new MenuItem({
-        role: 'windowMenu',
-    }),
-    new MenuItem({
-        label: 'Help',
-        submenu: [
-            {
-                label: 'Feedback',
-                click: handleFeedbackEvent,
-            },
-        ],
-    }),
-];
-
-const menu = Menu.buildFromTemplate(menuTemplate);
-Menu.setApplicationMenu(menu);
+if (process.platform === 'darwin') {
+    // MacOS
+    const menuTemplate = [
+        new MenuItem({
+            label: app.name,
+            submenu: [
+                {
+                    label: `About ${app.name}`,
+                    role: 'about',
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    label: 'Preference Settings',
+                    accelerator: 'command+,',
+                    click: handlePreferenceMenuEvent,
+                },
+                {
+                    label: 'Theme Settings',
+                    accelerator: 'command+t',
+                    click: handleThemeMenuEvent,
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    label: `Hide ${app.name}`,
+                    role: 'hide',
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    label: `Quit ${app.name}`,
+                    role: 'quit',
+                },
+            ],
+        }),
+        new MenuItem({
+            label: 'File',
+            submenu: [
+                {
+                    label: 'New Note',
+                    accelerator: 'command+n',
+                    click: handleNewNoteEvent,
+                },
+                {
+                    label: 'Search In Notebook',
+                    accelerator: 'command+shift+f',
+                    click: handleSearchNoteEvent,
+                },
+                {
+                    label: 'Close Note',
+                    accelerator: 'command+w',
+                    click: handleCloseNoteEvent,
+                },
+            ],
+        }),
+        new MenuItem({
+            label: 'Edit',
+            submenu: [
+                {
+                    role: 'undo',
+                },
+                {
+                    role: 'redo',
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    role: 'cut',
+                },
+                {
+                    role: 'copy',
+                },
+                {
+                    role: 'paste',
+                },
+                {
+                    role: 'selectAll',
+                },
+            ],
+        }),
+        new MenuItem({
+            label: 'View',
+            submenu: [
+                {
+                    label: 'Toggle Sidebar',
+                    accelerator: 'command+1',
+                    click: handleToggleSidebarEvent,
+                },
+                {
+                    label: 'Toggle Outline',
+                    accelerator: 'command+2',
+                    click: handleToggleOutlineEvent,
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    role: 'reload',
+                },
+                {
+                    role: 'forceReload',
+                },
+                {
+                    role: 'toggleDevTools',
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    role: 'resetZoom',
+                },
+                {
+                    role: 'zoomIn',
+                },
+                {
+                    role: 'zoomOut',
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    role: 'togglefullscreen',
+                },
+            ],
+        }),
+        new MenuItem({
+            role: 'windowMenu',
+        }),
+        new MenuItem({
+            label: 'Help',
+            submenu: [
+                {
+                    label: 'Feedback',
+                    click: handleFeedbackEvent,
+                },
+            ],
+        }),
+    ];
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
+} else {
+    // Windows/Linux
+    const menuTemplate = [
+        new MenuItem({
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Preference Settings',
+                    accelerator: 'ctrl+p',
+                    click: handlePreferenceMenuEvent,
+                },
+                {
+                    label: 'Theme Settings',
+                    accelerator: 'ctrl+t',
+                    click: handleThemeMenuEvent,
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    label: 'New Note',
+                    accelerator: 'ctrl+n',
+                    click: handleNewNoteEvent,
+                },
+                {
+                    label: 'Search In Notebook',
+                    accelerator: 'ctrl+shift+f',
+                    click: handleSearchNoteEvent,
+                },
+                {
+                    label: 'Close Note',
+                    accelerator: 'ctrl+w',
+                    click: handleCloseNoteEvent,
+                },
+            ],
+        }),
+        new MenuItem({
+            label: 'Edit',
+            submenu: [
+                {
+                    role: 'undo',
+                },
+                {
+                    role: 'redo',
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    role: 'cut',
+                },
+                {
+                    role: 'copy',
+                },
+                {
+                    role: 'paste',
+                },
+                {
+                    role: 'selectAll',
+                },
+            ],
+        }),
+        new MenuItem({
+            label: 'View',
+            submenu: [
+                {
+                    label: 'Toggle Sidebar',
+                    accelerator: 'ctrl+1',
+                    click: handleToggleSidebarEvent,
+                },
+                {
+                    label: 'Toggle Outline',
+                    accelerator: 'ctrl+2',
+                    click: handleToggleOutlineEvent,
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    role: 'reload',
+                },
+                {
+                    role: 'forceReload',
+                },
+                {
+                    role: 'toggleDevTools',
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    role: 'resetZoom',
+                },
+                {
+                    role: 'zoomIn',
+                },
+                {
+                    role: 'zoomOut',
+                },
+            ],
+        }),
+        new MenuItem({
+            label: 'Window',
+            submenu: [
+                {
+                    role: 'minimize',
+                },
+                {
+                    role: 'zoom',
+                },
+            ],
+        }),
+        new MenuItem({
+            label: 'Help',
+            submenu: [
+                {
+                    label: 'Feedback',
+                    click: handleFeedbackEvent,
+                },
+            ],
+        }),
+    ];
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
+}
