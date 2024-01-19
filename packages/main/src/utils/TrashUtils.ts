4face69a47833a69eae37cb18802c5a439cc93f4
xuelingkang <xuelingkang@163.com>
2024-01-19 20:49:57 +0800

    Pref: TrashUtils

--- packages/main/src/utils/TrashUtils.ts
+++ packages/main/src/utils/TrashUtils.ts
@@ -3,41 +3,48 @@ import path from 'node:path';
 import mainProcessService from '../service/MainProcessService';
 
 class TrashUtils {
-    private static assetsForClean = new Set<string>();
+    private static readonly MAX_LENGTH = 1000;
+    private static assetsForClean = new Array<string>();
 
     static add(assets: string[]): void {
+        TrashUtils.delete(assets);
         for (const asset of assets) {
-            TrashUtils.assetsForClean.add(asset);
+            TrashUtils.assetsForClean.push(asset);
+            // check length
+            while (TrashUtils.assetsForClean.length > TrashUtils.MAX_LENGTH) {
+                const deleted = TrashUtils.assetsForClean.splice(0, 1);
+                TrashUtils.unlink(deleted[0]);
+            }
         }
     }
 
     static delete(assets: string[]): void {
         for (const asset of assets) {
-            TrashUtils.assetsForClean.delete(asset);
+            const index = TrashUtils.assetsForClean.findIndex((value) => value === asset);
+            if (index < 0) {
+                continue;
+            }
+            TrashUtils.assetsForClean.splice(index, 1);
         }
     }
 
     static clean(): void {
-        const assetsRoot = mainProcessService.getAssetsRoot();
-        TrashUtils.assetsForClean.forEach((asset) => {
-            const url = path.join(assetsRoot, asset);
-            if (fs.existsSync(url)) {
-                fs.unlinkSync(url);
-            }
-        });
+        TrashUtils.assetsForClean.forEach(TrashUtils.unlink);
     }
 
     static cleanImmediately(assets: string[]): void {
         if (!assets || !assets.length) {
             return;
         }
+        assets.forEach(TrashUtils.unlink);
+    }
+
+    private static unlink(asset: string): void {
         const assetsRoot = mainProcessService.getAssetsRoot();
-        assets.forEach((asset) => {
-            const url = path.join(assetsRoot, asset);
-            if (fs.existsSync(url)) {
-                fs.unlinkSync(url);
-            }
-        });
+        const url = path.join(assetsRoot, asset);
+        if (fs.existsSync(url)) {
+            fs.unlinkSync(url);
+        }
     }
 }
 
